df7f27f1c171b4dff71064a894978ec1a9d5146d
purebloods <51003332+purebloods@users.noreply.github.com>
2019-07-23 23:16:17 +0530

    Update beg1.cpp

--- beg1.cpp
+++ beg1.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-	int c;
+	int c,n;
 	char a[50];
+	n=strlen(a);
 	cin.getline (a,50);
+	n=strlen(a);
 	cin>>c;
 	for (int i=0; a[i]!='\0';i++)
 	{
-		if (i==c-1)
+		if (i>n-c-1)
 		    cout<<a[i];
 	}
 	
 }
-
