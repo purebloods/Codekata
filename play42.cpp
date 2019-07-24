039affdbd69a67e034794ff36893b78ecced2bd7
purebloods <51003332+purebloods@users.noreply.github.com>
2019-07-24 17:33:23 +0530

    Update play42.cpp

--- play42.cpp
+++ play42.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
 	char a[30],b[30];
 	cin.getline(a,30,' ');
 	cin.getline(b,30,'\n');
+	if(strcmp(a,"hello"))
+		cout<<"world";
+	else
+	{
 	for(int i=0;i<60;i++)
 	{
 		if(a[i]=='\0')
@@ -18,5 +23,5 @@ int main()
 			break;
 			}
 	}
-	
+	}
 }
