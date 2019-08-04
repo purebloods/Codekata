457be40e6951ac54a5e9b979c3b2224058940c58
purebloods <51003332+purebloods@users.noreply.github.com>
2019-08-04 15:43:05 +0530

    Add files via upload

---
+++ beg16.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int prime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+      {
+          if(n%i==0)
+            return 0;
+      }
+    return 1;
+
+}
+int main()
+{
+    int l,r,m=0;
+    cin>>l>>r;
+    for(int i=l;i<r;i++)
+    {
+        if(prime(i) && m==0)
+        {
+            cout<<i;
+            m++;
+        }
+        else if(prime(i))
+            cout<<" " <<i;
+    }
+}
