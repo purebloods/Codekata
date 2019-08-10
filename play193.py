9acbecdbca1cc07d430866acf32c5fc6f04c6279
purebloods <51003332+purebloods@users.noreply.github.com>
2019-08-10 14:27:09 +0530

    Add files via upload

---
+++ play193.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Baru/AppData/Local/Programs/Python/Python37-32/play193.py 
+ 
+Traceback (most recent call last):
+  File "C:/Users/Baru/AppData/Local/Programs/Python/Python37-32/play193.py", line 1, in <module>
+    a=(int) (input(" "));
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+ RESTART: C:/Users/Baru/AppData/Local/Programs/Python/Python37-32/play193.py 
+ 3
+ 5
+ 2
+11
+>>> 
