4435971ae5089e512d3f16f5d7c417dc4f8b5324
purebloods <51003332+purebloods@users.noreply.github.com>
2019-08-10 14:43:31 +0530

    Update beg20.py

--- beg20.py
+++ beg20.py
@@ -1,10 +1,12 @@
 n=(int)(input())
 p=5
-for x in range(1,n+1):
-    p=p*x;
-    if(x==n):
-        print(p)
-    else:
-        print(p,end=' ')
-    p=5;
-    
+if(n==0):
+    print(0)
+else:
+    for x in range(1,n+1):
+        p=p*x;
+        if(x==n):
+            print(p)
+        else:
+            print(p,end=' ')
+        p=5;
