17dcef6bd74b2e5fb93dfc7e76c6fb5ba0384fad
purebloods <51003332+purebloods@users.noreply.github.com>
2019-08-12 19:04:15 +0530

    Create hunt29.cpp

---
+++ hunt29.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,maxi,sum=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    maxi=a[0];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<n-i;j++)
+        {
+            for(int k=i;k<i+j-1;k++)
+            {
+                sum+=a[k];
+            }
+            maxi= max(maxi,sum);
+            sum=0;
+        }
+
+    }
+    cout<<maxi;
+}
